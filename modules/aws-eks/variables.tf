ee8947235794fd0efc6dedd53709e1fd1eea157d
root <root@DESKTOP-EUS97HL>
2024-02-25 20:50:56 -0600

    feat: created eks cluster

---
+++ modules/aws-eks/variables.tf
@@ -0,0 +1,11 @@
+variables {
+    description = "Name of the Kubernetes cluster"
+    type = "string"
+    default = "aws_eks_01"
+}
+
+variables {
+    description = "cluster version"
+    type = "string"
+    default = "1.24"
+}
